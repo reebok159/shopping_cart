dfbf75cbfd0a39b10338a4ddb5c41766c544250e
Eugene V <kokokoi9@mail.ru>
2018-04-10 13:55:36 +0300

    fix migrations

--- db/migrate/20180410105137_create_shopping_cart_order_items.rb
+++ db/migrate/20180410105137_create_shopping_cart_order_items.rb
@@ -3,7 +3,7 @@ class CreateShoppingCartOrderItems < ActiveRecord::Migration[5.1]
     create_table :shopping_cart_order_items do |t|
       t.integer :quantity
       t.belongs_to :order, foreign_key: { to_table: :shopping_cart_orders }, index: true
-      t.belongs_to :product, foreign_key: true, index: true
+      t.integer :product_id, foreign_key: true
       t.timestamps
     end
   end
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180317212909) do
+ActiveRecord::Schema.define(version: 20180410105137) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -71,7 +71,6 @@ ActiveRecord::Schema.define(version: 20180317212909) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["order_id"], name: "index_shopping_cart_order_items_on_order_id"
-    t.index ["product_id"], name: "index_shopping_cart_order_items_on_product_id"
   end
 
   create_table "shopping_cart_orders", force: :cascade do |t|
